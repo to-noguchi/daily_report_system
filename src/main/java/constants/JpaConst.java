106e4b9b86f68bc4deb7aa408ce3d3216668e2d0
tomoyo.noguchi <azalea5.hydrangea6@gmail.com>
2022-11-04 16:10:51 +0900

    Lesson 17Chapter 9.1　DTOモデル（Reportクラス）部分

--- src/main/java/constants/JpaConst.java
+++ src/main/java/constants/JpaConst.java
@@ -40,9 +40,17 @@ public interface JpaConst {
     String REP_COL_CREATED_AT = "created_at"; //登録日時
     String REP_COL_UPDATED_AT = "updated_at"; //更新日時
 
+    //フォローテーブル
+    String TABLE_EMPFOL = "employee_follows"; //テーブル名
+    //フォローテーブルカラム
+    String EMPFOL_COL_ID = "id"; //id
+    String EMPFOL_COL_EMP = "employee_id"; //フォローされる従業員のid
+    String EMPFOL_COL_FOL = "follower_id"; //フォローする従業員のid
+
     //Entity名
     String ENTITY_EMP = "employee"; //従業員
     String ENTITY_REP = "report"; //日報
+    String ENTITY_EMPFOL = "employee_follow"; //フォロー
 
     //JPQL内パラメータ
     String JPQL_PARM_CODE = "code"; //社員番号
@@ -74,5 +82,11 @@ public interface JpaConst {
     //指定した従業員が作成した日報の件数を取得する
     String Q_REP_COUNT_ALL_MINE = ENTITY_REP + ".countAllMine";
     String Q_REP_COUNT_ALL_MINE_DEF = "SELECT COUNT(r) FROM Report AS r WHERE r.employee = :" + JPQL_PARM_EMPLOYEE;
+    //フォローしている全ての従業員をidの降順で取得する
+    String Q_EMPFOL_GET_ALL = ENTITY_EMPFOL + ".getAll";
+    String Q_EMPFOL_GET_ALL_DEF = "SELECT ef FROM Employee_Follow AS ef ORDER BY ef.id DESC";
+    //フォローしている全ての従業員の件数を取得する
+    String Q_EMPFOL_COUNT = ENTITY_EMPFOL + ".count";
+    String Q_EMPFOL_COUNT_DEF = "SELECT COUNT(ef) FROM Employee_Follow AS ef";
 
 }
